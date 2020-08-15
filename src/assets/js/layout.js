70b22922fe77d93135b35776823cd7f793ab6fc8
sonhoang1809 <sonhoang1809@gmail.com>
2020-08-15 20:35:16 +0700

    new interface

---
+++ src/assets/js/layout.js
@@ -0,0 +1,18 @@
+function sitebarItem (label, level, href, subItem){
+	
+	var this = {};
+
+	this.label = label;
+	this.level = level;
+	this. href = href;
+	this.subItem = subItem; 
+
+	this.generateHTML = function (){
+
+		
+	}
+
+}
+
+
+
