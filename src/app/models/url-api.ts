068bd44e97d719bed5ce3c9ba17176c1d8123c92
sonhoang1809 <sonhoang1809@gmail.com>
2020-08-24 17:32:58 +0700

    Change URLServer

--- src/app/models/url-api.ts
+++ src/app/models/url-api.ts
@@ -1,6 +1,6 @@
-export const UrlServer = "http://localhost:9000";
-//export const UrlServer = "https://managesachfruits-backend.herokuapp.com";
-
+//export const UrlServer = "http://localhost:9000";
+export const UrlServer = "https://managesachfruits-backend.herokuapp.com";
+ 
 
 
 
@@ -10,10 +10,13 @@ export const UrlServerAPISummaryCostInMonth = UrlServer+'/api/Moneys/SummaryCost
 export const UrlServerAPISummaryProfitInMonth = UrlServer+'/api/Moneys/SummaryProfitsInMonth';
 export const UrlServerAPISummaryRevenuePerOrderInMonth = UrlServer+'/api/Moneys/SummaryRevenuePerOrderInMonth';
 
+
 export const UrlServerAPISummaryNumberOrderInMonth = UrlServer+'/api/orders/NumberOrder';
 export const UrlServerAPISummaryNumberCostInMonth = UrlServer+'/api/costs/NumberCost';
 export const UrlServerAPISummaryNumberProductInStockInMonth = UrlServer+'/api/products/ProductInStockInMonth';
 export const UrlServerAPIStatisticsRevenueCost = UrlServer+'/api/Moneys/Statistics/Revenue/Cost';
+
+
 
 export const UrlServerAPISearchOrder = UrlServer+'/api/Orders/Search';
 export const UrlServerAPISearchCost = UrlServer+'/api/Costs/Search';
